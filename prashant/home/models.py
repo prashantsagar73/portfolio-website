f1eec340d2d5a6989ec546a5f1d9d542100b24e6
Prashant sagar <prashant.sagar73@gmail.com>
2021-06-21 00:18:09 +0530

    timeline data updated

--- prashant/home/models.py
+++ prashant/home/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class Timeline(models.Model):
     post_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=300,default=" ")
+    title = models.CharField(max_length=300)
     subtitle = models.CharField(max_length=500,default=" ")
     content = models.TextField(default=" ")
     timestamp = models.DateField(default=timezone.now)
@@ -12,5 +12,5 @@ class Timeline(models.Model):
         unique=False, 
         blank=True)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title 
