8bfbaf8f306428d2f7cfbf83b03fb9123ee3a6ef
prashantsagar73 <prashant.sagar73@gmail.com>
2021-07-14 00:31:24 +0530

    modify code

--- prashant/prashant/settings.py
+++ prashant/prashant/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['prashanttech.herokuapp.com']
+# ALLOWED_HOSTS = ['prashanttech.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definitionclear
@@ -123,7 +124,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),) # new
+# STATIC_ROOT = BASE_DIR /'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
---
+++ readme.md
@@ -0,0 +1,12 @@
+Prashant's sagar personal website
+
+
+You can find a live version at [prashantsagar.tech](prashanttech.herokuapp.com).
+
+## Frameworks and packages used
+
+- [Django](https://www.djangoproject.com/) (Based on [Python](https://www.python.org/))
+- [Tailblock](https://tailblocks.cc/)
+- [Bootstrap](https://getbootstrap.com/) - for blog page
+- [javascript](https://www.javascript.com/)
+- ... and more!
